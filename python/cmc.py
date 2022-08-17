718f9e0cb51d07f19a837e03ca6d576ae4513dc4
Armadillan <74256936+Armadillan@users.noreply.github.com>
2022-08-17 10:05:21 +0200

    gameover in python cmc
    nevermind, far from done

--- python/cmc.py
+++ python/cmc.py
@@ -16,6 +16,11 @@ class Grid:
 
         return new
 
+    def reset(self):
+        self.__init__()
+        self.new_tile()
+        self.new_tile()
+
     def rotate(self, n):
         self.grid = np.rot90(grid, n)
 
@@ -62,8 +67,33 @@ class Grid:
         self.compress()
         self.rotate((4 - direction) % 4)
 
+        #todo
+        #generate new tiles and check for gameover
+
+    #property for compatibility with existing frontent
+    @property
+    def check_gameover(self):
+
+        if not self.state.all():
+            return False
+
+        for y in range(3):
+            for x in range(4):
+                if self.state[y, x] == self.state[y+1, x]:
+                    return False
+        for y in range(4):
+            for x in range(3):
+                if self.state[y, x] == self.state[y, x+1]:
+                    return False
+        return True
+
+    #for compatibility with existing frontent
+    self.step = self.move
+    self.state = self.grid
+
     def pprint(self):
-        # 7x7 chars for grid squares,
+        # 7x7 chars for grid squares
+        #todo
         pass
 
 
