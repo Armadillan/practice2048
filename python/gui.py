a74bad663e718b1a24e92e2c927331b5dce76e5f
Antoni <74256936+Armadillan@users.noreply.github.com>
2022-08-26 10:21:32 +0200

    notes and comment cleanup

--- python/gui.py
+++ python/gui.py
@@ -302,12 +302,8 @@ class Interface:
         pygame.quit()
 
 if __name__ == "__main__":
-    # Plays the game without a bot attached :)
-    # from env import PyEnv2048
     from cmc import Game
 
-    # Creates Game object, passing an environment to the constructor
-    # game = Game(PyEnv2048())
     game = Interface(Game())
-    # Starts the interface
+
     game.main()
