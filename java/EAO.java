a58053f7532d453829cddbc6898b9bc939f5aaee
Armadillan <74256936+Armadillan@users.noreply.github.com>
2022-10-03 10:58:44 +0200

    finish EAO.java

--- java/EAO.java
+++ java/EAO.java
@@ -1,3 +1,164 @@
-public class EAO {
+import java.util.ArrayList;
+import java.util.Random;
 
+import helpers.MoveReturn;
+import helpers.RotateLongMatrix;
+import helpers.Direction;
+import helpers.GameInterface;
+
+public class EAO implements GameInterface {
+    private long[][] grid;
+    private long score;
+    private Random rng = new Random();
+
+    public EAO() {
+        // initalized with default value 0
+        grid = new long[4][4];
+        score = 0;
+    }
+
+    public void reset() {
+        grid = new long[4][4];
+        new_tile();
+        new_tile();
+        score = 0;
+    }
+
+    public long[][] getGrid() {
+        return grid;
+    }
+
+    public long getScore() {
+        return score;
+    }
+
+    private void new_tile() {
+        // find all empty tiles
+        var empty = new ArrayList<int[]>();
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                if (grid[x][y] == 0) {
+                    int[] val = {x, y};
+                    empty.add(val);
+                }
+            }
+        }
+
+        var new_tile = empty.get(rng.nextInt(empty.size()));
+
+        long new_val = 2;
+        if (rng.nextLong() > 0.8) new_val = 4;
+        grid[new_tile[0]][new_tile[1]] = new_val;
+    }
+
+    private void rotate_90() {
+        RotateLongMatrix.rotate(grid);
+    }
+
+    private void rotate(int n) {
+        for (int i = 0; i<n; i++) {
+            rotate_90();
+        }
+    }
+
+    public boolean gameover() {
+
+        for (int i = 0; i < 4; i++) {
+            // for each val in grid[i]
+            for (long val: grid[i]) {
+                if (val == 0) {
+                    return false;
+                }
+            }
+        }
+
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 4; x++) {
+                if (grid[x][y] == grid[x][y+1]) {
+                     return false;
+                }
+            }
+        }
+
+        for (int y = 0; y<4; y++) {
+            for (int x = 0; x < 3; x++) {
+                if (grid[x][y] == grid[x+1][y]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private MoveReturn move(Direction direction) {
+        boolean moved = false;
+        long reward = 0;
+        var merged = new ArrayList<int[]>();
+
+        int rotate_num;
+        switch (direction) {
+            case Up: rotate_num = 3; break;
+            case Right: rotate_num = 2; break;
+            case Down: rotate_num = 1; break;
+            case Left:
+            default:
+                rotate_num = 0; break;
+        }
+        rotate(rotate_num);
+
+        for (int x = 0; x < 4; x++) {
+            for (int y = 1; y < 4; y++) {
+                long tile_val = grid[x][y];
+
+                if (tile_val > 0) {
+
+                    int new_y = y;
+
+                    while (new_y > 0 && grid[x][new_y-1] == 0) {
+                        new_y -= 1;
+                    }
+
+                    int[] potential_mege_coord = {x, new_y-1};
+
+                    if (
+                        new_y > 0
+                        && tile_val == grid[x][new_y-1]
+                        && !merged.contains(potential_mege_coord)
+                        ) {
+                            grid[x][y] = 0;
+                            grid[x][new_y-1] *= 2;
+                            merged.add(potential_mege_coord);
+                            reward += tile_val * 2;
+                            moved = true;
+
+                        } else if (new_y != y) {
+                            grid[x][y] = 0;
+                            grid[x][new_y] = tile_val;
+                            moved = true;
+                        }
+                }
+            }
+        }
+
+        rotate((4-rotate_num) % 4);
+        return new MoveReturn(moved, reward);
+    }
+
+    public long step(Direction direction) {
+        if (gameover()) {
+            reset();
+            return 0;
+        }
+
+        MoveReturn move_out = move(direction);
+
+        if (move_out.moved()) {
+            new_tile();
+        }
+
+        score += move_out.reward();
+
+        return move_out.reward();
+    }
 }
--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        CMC grid = new CMC();
+        // CMC grid = new CMC();
+        EAO grid = new EAO();
         Interface game = new Interface(grid);
         game.main();
     }
